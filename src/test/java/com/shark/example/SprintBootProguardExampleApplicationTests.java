de1bcc3dd0c98a874c345dccefcda991256d2757
Shark0 <lin22223344@gmail.com>
2019-12-19 15:00:40 +0800

    基本Spring Boot Application混淆

---
+++ src/test/java/com/shark/example/SprintBootProguardExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shark.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootProguardExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
